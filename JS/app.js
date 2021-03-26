d236300ff09f7f34598bff2895ef1cd25afb305e
jayant <jayantparihar@yahoo.com>
2021-03-26 16:25:17 -0700

    added the todoList array

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,9 @@
-// your JavaScript goes here, you can delete this comment
+const todoList = [
+    {todo: "clone repo for starter code", status: "complete"},
+    {todo: "add css and js links to index", status: "complete"},
+    {todo: "complete the app.js", status: "started"},
+    {todo: "finish the lab week 11", status: "started"}
+]
+const completedTodos = []
+
+
